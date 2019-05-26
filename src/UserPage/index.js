5801810d02c1b8e29ca05e53573d7d0a9116f557
Krishna Pagadala <prakis@gmail.com>
2019-05-26 15:21:24 +0530

    Seperate css functionality removed

--- src/UserPage/index.js
+++ src/UserPage/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import {
-    withRouter
-} from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import Mustache from 'mustache';
 import Parser from 'html-react-parser';
 //import DOMPurify from 'dompurify';
@@ -10,108 +8,113 @@ const GUC = "githubusercontent.com";
 const GITHACK = "githack.com";
 const GITCDN_LINK = "gitcdn.link";
 class UserPage extends React.Component {
-        constructor(props) {
-            super(props);
+    constructor(props) {
+        super(props);
 
-            var fullPath = this.props.location.pathname;
-            var username = fullPath.split('/')[1];
+        var fullPath = this.props.location.pathname;
+        var username = fullPath.split('/')[1];
 
-            this.state = {
-                gitHubUserName: username,
-                isValidGitHubUserName: true,
-                profileJson: '',
-                themeTemplate: '',
-                themeUrlRoot: ''
-            };
-            this.getProfileJson();
+        this.state = {
+            gitHubUserName: username,
+            isValidGitHubUserName: true,
+            profileJson: '',
+            themeTemplate: '',
+            themeUrlRoot: ''
+        };
+        this.getProfileJson();
+    }
+    addOtherJsonValues(json) {
+        json.basics['github'] = "https://github.com/" + this.state.gitHubUserName;
+        let picName = json.basics['picture'];
+        json.basics['picture_fullpath'] = this.getGitCachePath(GUC) + picName;
+        console.log(json.basics['picture_fullpath']);
+        this.setState({
+            profileJson: json
+        });
+    }
+    getGitCachePath(cdn, repo = "stringify.me") {
+        var path = "";
+        switch (cdn) {
+            case GITCDN_LINK:
+                path = 'https://gitcdn.link/repo/' + this.state.gitHubUserName + '/' + repo + '/master/';
+                break;
+            case GITHACK:
+                path = 'https://raw.githack.com/' + this.state.gitHubUserName + '/' + repo + '/master/';
+                break;
+            default:
+                path = 'https://raw.githubusercontent.com/' + this.state.gitHubUserName + '/' + repo + '/master/';
         }
-        addOtherJsonValues(json) {
-            json.basics['github'] = "https://github.com/" + this.state.gitHubUserName;
-            let picName = json.basics['picture'];
-            json.basics['picture_fullpath'] = this.getGitCachePath(GUC) + picName;
-            console.log(json.basics['picture_fullpath']);
-            this.setState({
-                profileJson: json
+        return path;
+    }
+    getProfileJson() {
+        const profileJsonURL = this.getGitCachePath(GITHACK) + 'profile.json';
+        console.log(profileJsonURL);
+        var me = this;
+        fetch(profileJsonURL)
+            .then(response => response.json())
+            .then(json => {
+
+                me.addOtherJsonValues(json);
+
+                if (json && json.theme) {
+                    me.getTemplate(json.theme);
+                }
+
+            }).catch(error => {
+                this.setState({
+                    isValidGitHubUserName: false
+                })
+                console.log("GitHub User profile error", error);
             });
-        }
-        getGitCachePath(cdn, repo = "stringify.me") {
-            var path = "";
-            switch (cdn) {
-                case GITCDN_LINK:
-                    path = 'https://gitcdn.link/repo/' + this.state.gitHubUserName + '/' + repo + '/master/';
-                    break;
-                case GITHACK:
-                    path = 'https://raw.githack.com/' + this.state.gitHubUserName + '/' + repo + '/master/';
-                    break;
-                default:
-                    path = 'https://raw.githubusercontent.com/' + this.state.gitHubUserName + '/' + repo + '/master/';
-            }
-            return path;
-        }
-        getProfileJson() {
-            const profileJsonURL = this.getGitCachePath(GITHACK) + 'profile.json';
-            console.log(profileJsonURL);
-            var me = this;
-            fetch(profileJsonURL)
-                .then(response => response.json())
-                .then(json => {
+    }
+    /*appendTemplateCSSPath(template) {
+        const cdnpath = this.state.themeUrlRoot;
+        const templatecss = "template.css";
+        console.log(cdnpath + templatecss);
+        return template.replace(templatecss, cdnpath + templatecss);
+    }*/
+    getTemplate(theme) {
+        // https://github.com/prakis/stringify.me-theme
+        // https://raw.githubusercontent.com/prakis/stringify.me-theme/master/theme.template
+        //let themeUrlBase = theme.url.replace("//github.com/", "//raw.githubusercontent.com/");
+        let themeUrlBase = theme.url.replace("//github.com/", "//raw.githack.com/");
+        //let themeUrlBase = theme.url.replace("//github.com/", "//raw.githack.com/");
+        const themeUrlRoot = themeUrlBase + "/master/";
+        this.setState({
+            themeUrlRoot: themeUrlRoot
+        });
+        //let themeUrlRoot = "https://raw.githack.com/prakis/stringify.me-theme-card2/master/";
+        var fullTemplatePath = themeUrlRoot + "template.html";
+        console.log(fullTemplatePath);
 
-                    me.addOtherJsonValues(json);
+        fetch(fullTemplatePath)
+            .then(response => response.text())
+            .then(templateData => {
+                //let withCSSPath = this.appendTemplateCSSPath(data);
 
-                    if (json && json.theme) {
-                        me.getTemplate(json.theme);
-                    }
-
-                }).catch(error => {
-                    this.setState({
-                        isValidGitHubUserName: false
-                    })
-                    console.log("GitHub User profile error", error);
+                this.setState({
+                    themeTemplate: templateData
                 });
-        }
-        appendMainCSSPath(template) {
-            const cdnpath = this.state.themeUrlRoot;
-            const maincss = "main.css";
-            console.log(cdnpath + maincss);
-            return template.replace(maincss, cdnpath + maincss);
-        }
-        getTemplate(theme) {
-            // https://github.com/prakis/stringify.me-theme
-            // https://raw.githubusercontent.com/prakis/stringify.me-theme/master/theme.template
-            //let themeUrlBase = theme.url.replace("//github.com/", "//raw.githubusercontent.com/");
-            let themeUrlBase = theme.url.replace("//github.com/", "//raw.githack.com/");
-            //let themeUrlBase = theme.url.replace("//github.com/", "//raw.githack.com/");
-            const themeUrlRoot = themeUrlBase + "/master/";
-            this.setState({
-                themeUrlRoot: themeUrlRoot
+
+            }).catch(error => {
+                //this.setState({ isValidGitHubUserName: false });
+                console.log("GitHub Theme error", error);
             });
-            //let themeUrlRoot = "https://raw.githack.com/prakis/stringify.me-theme-card2/master/";
-            const fullTemplatePath = themeUrlRoot + "template.html";
-            console.log(fullTemplatePath);
-
-            fetch(fullTemplatePath)
-                .then(response => response.text())
-                .then(data => {
-                    let withCSSPath = this.appendMainCSSPath(data);
-
-                    this.setState({
-                        themeTemplate: withCSSPath
-                    });
-
-                }).catch(error => {
-                    //this.setState({ isValidGitHubUserName: false });
-                    console.log("GitHub Theme error", error);
-                });
+    }
+    render() {
+        //let template = JSON.stringify(this.state.themeTemplate);
+        //const stylelink = this.state.themeUrlRoot ? <link rel="stylesheet" type="text/css" href={this.state.themeUrlRoot + "template.css"} /> : null;
+        var __html = null;
+        if(this.state.themeTemplate){
+            const renderedTemplate = Mustache.render(this.state.themeTemplate, this.state.profileJson);
+            __html = (this.state.themeTemplate) ? <div> {Parser(renderedTemplate)} </div> : null;
         }
-        render() {
-            //let template = JSON.stringify(this.state.themeTemplate);
-            const __html = Mustache.render(this.state.themeTemplate, this.state.profileJson);
-            //console.log(">", __html, "<");
-            let divref = (__html) ? <div ref = 'test' > { Parser(__html) } </div> : null;
-            //<div ref='test' dangerouslySetInnerHTML={{  __html: DOMPurify.sanitize(__html)   }} />
-            //return ( <div > { divref } </div>);
-            return (  divref );
-            }
-        }
+        //console.log(">", __html, "<");
+        let divref = <>
+            {__html}
+        </>;
+        return (divref);
+    }
+}
 
-        export default withRouter(UserPage);
+export default withRouter(UserPage);
